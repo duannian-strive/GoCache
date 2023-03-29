1d6f06936b9bb0600a63a2e37e2f7d83fd313e21
duanniannian <duanniannian1232@gmail.com>
2023-03-29 17:24:27 +0800

    Update one.go

--- one.go
+++ one.go
@@ -1,12 +1,3 @@
 package main
 
 const one = 1
-const nam = "dnner"
-const two = 2
-const three = 3
-const ot = 711
-const two2 = 323
-const name = "nan"
-const six = 62334334
-const ten = 10
-const namee = 2
